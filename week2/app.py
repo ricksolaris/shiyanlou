88fcee03498513a0825cfd13bed1e5dacb724044
ricksolaris <dongqian3@qq.com>
2018-06-25 15:47:09 +0800

    week2:use Jinja

--- week2/app.py
+++ week2/app.py
@@ -1,12 +1,30 @@
 #! /usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD']
 
 @app.route('/')
 def index():
-    return 'Hello Dongqian!'
+
+    teacher = {
+        'name': 'Jack',
+        'email': 'dongqian3@qq.com'
+    }
+
+    course = {
+        'name': 'Python Basic',
+        'teacher': teacher,
+        'user_count': 5348,
+        'price': 199.0,
+        'lab': None,
+        'is_private': False,
+        'is_member_course': True,
+        'tags': ['python','big data','linux']
+    }
+    #return 'Hello Dongqian!'
+    return render_template('index.html', course=course)
 
 if __name__ == '__main__':
     app.run()

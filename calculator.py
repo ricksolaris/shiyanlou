fb79ec1a6d8f9988ffde69ee077a047379a4efba
董骞 <dongqian3@qq.com>
2018-07-20 13:18:04 +0800

    挑战3

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import csv
+from collections import namedtuple
 
+IncomeTaxQuickLookupItem = namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point', 'tax_rate', 'quick_subtractor']
+)
+
+INCOME_TAX_START_POINT = 3500
+
+INCOME_TAX_QUICK_LOOKUP_TABLE = [
+    IncomeTaxQuickLookupItem(80000, 0.45, 13505),
+    IncomeTaxQuickLookupItem(55000, 0.35, 5505),
+    IncomeTaxQuickLookupItem(35000, 0.30, 2755),
+    IncomeTaxQuickLookupItem(9000, 0.25, 1005),
+    IncomeTaxQuickLookupItem(4500, 0.20, 555),
+    IncomeTaxQuickLookupItem(1500, 0.10, 105),
+    IncomeTaxQuickLookupItem(0, 0.03, 0)
+]
 #dict_tax2 = {0:[0,0,0.03],1:[1500,105,0.1],2:[4500,555,0.2],3:[9000,1005,0.25],4:[35000,2755,0.3],5:[55000,5505,0.35],6:[80000,13505,0.45]}
 
 class Args(object):
@@ -9,34 +26,24 @@ class Args(object):
     def __init__(self):
         self.args = sys.argv[1:]
         #print(self.args)
-        def get_config(self):
-            index = self.args.index('-c')
-            configfile = self.args[index + 1]
-            # print(configfile)
-            return self._configfile
-
-        def get_userdata(self):
-            index = self.args.index('-d')
-            userfile = self.args[index + 1]
-            return self._userfile
-
-        def set_outfile(self):
-            index = self.args.index('-o')
-            outfile = self.args[index + 1]
-            return self._outfile
-
+    def _value_after_option(self, option):
         try:
-            index = self.args.index('-c')
-            configfile = self.args[index+1]
-            #print(configfile)
-            index = self.args.index('-d')
-            userfile = self.args[index+1]
-            index = self.args.index('-o')
-            outfile = self.args[index + 1]
-            #print(configfile,userfile,outfile)
-        except ValueError:
+            index = self.args.index(option)
+            return self.args[index + 1]
+        except (ValueError, IndexError):
             print('Parameter Error')
             exit()
+    @property
+    def config_path(self):
+        return self._value_after_option('-c')
+    @property
+    def userdata_path(self):
+        return self._value_after_option('-d')
+    @property
+    def export_path(self):
+        return self._value_after_option('-o')
+
+args = Args()
 
 class Config(object):
 
@@ -44,61 +51,114 @@ class Config(object):
         self.config = self._read_config()
 
     def _read_config(self):
-        with open(self) as file:
-            config = file.read()
+        config_path = args.config_path
+        config = {}
+        with open(config_path) as f:
+            for line in f.readlines():
+                key, value = line.strip().split(' = ')
+                try:
+                    config[key.strip()] = float(value.strip())
+                except ValueError:
+                    print('Parameter Error where read_config ')
+                    exit()
+        return config
+
+    def _get_config(self, key):
+        try:
+            return self.config[key]
+        except KeyError:
+            print('Config Error')
+            exit()
+
+    @property
+    def social_insurance_baseline_low(self):
+        return self._get_config('JiShuL')
+
+    @property
+    def social_insurance_baseline_high(self):
+        return self._get_config('JiShuH')
+
+    @property
+    def social_insurance_total_rate(self):
+        return sum([
+            self._get_config('YangLao'),
+            self._get_config('YiLiao'),
+            self._get_config('ShiYe'),
+            self._get_config('GongShang'),
+            self._get_config('ShengYu'),
+            self._get_config('GongJiJin')
+        ])
+
+config = Config()
+#print(config.social_insurance_total_rate)
+
+class UserData(object):
+
+    def __init__(self):
+        self.userdata = self._read_users_data()
+
+    def _read_users_data(self):
+        userdata_path = args.userdata_path
+        userdata = []
+        with open(userdata_path) as f:
+            for line in f.readlines():
+                employee_id, income_string = line.strip().split(',')
+                try:
+                    income = int(income_string)
+                except ValueError:
+                    print('Parameter Error @ read_users_data ')
+                    exit()
+                userdata.append((employee_id,income))
+        return userdata
+    
+    def __iter__(self):
+        return iter(self.userdata)
+
+class IncomeTaxCalculator(object):
+
+    def __init__(self,userdata):
+        self.userdata = userdata
+
+    @staticmethod
+    def calc_social_insurance_money(income):
+        if income < config.social_insurance_baseline_low:
+            return config.social_insurance_baseline_low * config.social_insurance_total_rate
+        if income > config.social_insurance_baseline_high:
+            return config.social_insurance_baseline_high * config.social_insurance_total_rate
+        return income * config.social_insurance_total_rate
+
+    @classmethod
+    def calc_income_tax_and_remain(cls, income):
+        social_insurance_money = cls.calc_social_insurance_money(income)
+        real_income = income - social_insurance_money
+        taxable_part = real_income - INCOME_TAX_START_POINT
+        if taxable_part <= 0:
+            return '0.00', '{:.2f}'.format(real_income)
+        for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+            if taxable_part > item.start_point:
+                tax = taxable_part * item.tax_rate - item.quick_subtractor
+                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
+
+    def calc_for_all_userdata(self):
+        result = []
+        for employee_id, income in self.userdata:
+            data = [employee_id, income]
+            social_insurance_money = '{:.2f}'.format(self.calc_social_insurance_money(income))
+            tax, remain = self.calc_income_tax_and_remain(income)
+            data += [social_insurance_money, tax, remain]
+            result.append(data)
+        return result
+
+    def export(self, file_type = 'csv'):
+        result = self.calc_for_all_userdata()
+        with open(args.export_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(result)
 
 
-def tax_calc(salary):
-    taxable_part = salary - 3500 - salary*0.165
-    if taxable_part <= 0:
-        return '0.00'
-    insurance_list = [
-        (80000, 0.45, 13505),
-        (55000, 0.35, 5505),
-        (35000, 0.30, 2755),
-        (9000, 0.25, 1005),
-        (4500, 0.2, 555),
-        (1500, 0.1, 105),
-        (0, 0.03, 0)
-    ]
-    for insurance in insurance_list:
-        #print(insurance[0],insurance[1])
-        if taxable_part > insurance[0]:
-            result = taxable_part*insurance[1]-insurance[2]
-            return '{:.2f}'.format(result)
-        #return sum_insurance
-        #print(sum_insurance)
-def main():
-    # if len(sys.argv) != 2:
-    #     print('Parameter Error')
-    #     exit()
-    try:
-        for arg in sys.argv[1:]:
-            salary_info = arg.split(':')
-            #print(salary_info)
-            #salary_id = salary_info[0]
-            #salary = int(salary_info[1])
-            #after_tax = float(salary) - float(salary * 0.165) - float(tax_calc(salary))
-            #print(str(salary_id)+':'+'{:.2f}'.format(after_tax))
-    except ValueError:
-        print('Parameter Error')
-        exit()
-    #print(tax_calc(salary))
+
 
 
 if __name__ == '__main__':
-    #main()
-    root = Args()
-    cfg = Config()
-    cfg_file = cfg.get_config()
-
-# for arg in sys.argv[1:]:
-#     #print(arg)
-#     #print(arg.split(':'))
-#     #tax_salary = float()
-#     user_info = arg.split(':')
-#     user_id = user_info[0]
-#     user_salary = float(user_info[1])
-#     print(user_id,user_salary)
-#     tax_calc(user_salary)
-
+    calculator = IncomeTaxCalculator(UserData())
+    calculator.export()

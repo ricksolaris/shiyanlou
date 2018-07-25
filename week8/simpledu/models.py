d3c324f78b41b9cfa45447f1ede61235cc0687f8
ricksolaris <dongqian3@qq.com>
2018-07-25 22:41:19 +0800

    创建管理后台框架

--- week8/simpledu/models.py
+++ week8/simpledu/models.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python3
-
 from datetime import datetime
-from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
-app.config.update(dict(
-    SECRET_KEY = 'very secret key',
-    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
-))
-
-db = SQLAlchemy(app)
+# 注意这里不再传入 app 了
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -27,20 +18,7 @@ class Course(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, index=True, nullable=False)
-
     author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    author = db.relationship('User', uselist=False)
+    author= db.relationship('User', uselist=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-@app.route('/')
-def index():
-    courses = Course.query.all()
-    return render_template('index.html', courses=courses)
-
-@app.route('/admin')
-def admin_index():
-    return 'admin'
-
-if __name__ == '__main__':
-    app.run()

0b0197c31aa21bbdd7a1904a621b13ee40fb23ff
ricksolaris <dongqian3@qq.com>
2018-07-31 22:34:09 +0800

    import Bootstrap

--- week8/simpledu/app.py
+++ week8/simpledu/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from simpledu.config import configs
 from simpledu.models import db, Course
 
+def register_blueprints(app):
+    from .handlers import front, course, admin
+    app.register_blueprint(front)
+    app.register_blueprint(course)
+    app.register_blueprint(admin)
 
 def create_app(config):
     """ 可以根据传入的 config 名称，加载不同的配置
@@ -10,16 +15,6 @@ def create_app(config):
     app.config.from_object(configs.get(config))
     # SQLAlchemy 的初始化方式改为使用 init_app
     db.init_app(app)
-
-    # 路由函数暂时写在这里，后面会介绍使用 Flask 的 Blueprint 实现
-    # 路由的模块化
-    @app.route('/')
-    def index():
-        courses = Course.query.all()
-        return render_template('index.html', courses=courses)
-
-    @app.route('/admin')
-    def admin_index():
-        return 'admin'
-
+    register_blueprints(app)
+    
     return app

c393f9fd5935936fcf042dc46e4ccb46d3219d73
董骞 <dongqian3@qq.com>
2018-08-02 18:46:57 +0800

    实现登录功能

--- week8/simpledu/app.py
+++ week8/simpledu/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, Blueprint
 from simpledu.config import configs
 from simpledu.models import db, Course
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from simpledu.models import db, User
 
 def register_blueprints(app):
     from .handlers import front, course, admin, user
@@ -16,8 +18,21 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     # SQLAlchemy 的初始化方式改为使用 init_app
-    db.init_app(app)
-    Migrate(app, db)
+    register_extensions(app)
     register_blueprints(app)
     
     return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
--- week8/simpledu/handlers/front.py
+++ week8/simpledu/handlers/front.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 from simpledu.models import Course
 from simpledu.forms import LoginForm, RegisterForm
+from flask import flash
+from flask import redirect, url_for
+from flask_login import login_user
+from simpledu.models import User
 
 
 front = Blueprint('front', __name__)
@@ -10,12 +14,20 @@ def index():
     courses = Course.query.all()
     return render_template('index.html', courses=courses)
 
-@front.route('/login')
+@front.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.index'))
     return render_template('login.html', form=form)
 
-@front.route('/register')
+@front.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('注册成功，请登录！', 'success')
+        return redirect(url_for('.login'))
     return render_template('register.html', form=form)
